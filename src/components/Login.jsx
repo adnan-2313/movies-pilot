5254ba68227bbad6c4b2ac4450f3aece490dd2a3
Adnan <ak567845@gmail.com>
2024-10-25 21:44:16 +0530

    added form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
-  const [isSignIn, SetIsSign] = useState(true);
+  const [isSignIn, setIsSign] = useState(true);
+  const [error, setError] = useState(null);
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const toggleSignForm = () => {
-    SetIsSign(!isSignIn);
+    setIsSign(!isSignIn);
   };
+
+  const handleButtonClick = () => {
+    const val = checkValidateData(
+      emailRef.current.value,
+      passwordRef.current.value
+    );
+    setError(val);
+  };
+
   return (
     <>
       <div className="flex justify-center bg-[#00000088] h-[100vh]">
@@ -18,7 +33,10 @@ const Login = () => {
           />
         </div>
         <div className="w-full  flex justify-center items-center">
-          <form className="relative w-[34%] max-w-[31.25rem] rounded-md mt-[100px]  text-white flex flex-col   bg-[#000000b2] p-12">
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="relative w-[34%] max-w-[31.25rem] rounded-md mt-[100px]  text-white flex flex-col   bg-[#000000b2] p-12"
+          >
             <h1 className="text-3xl font-bold m-2">
               {isSignIn ? "Sign In" : "Sign Up"}
             </h1>
@@ -26,26 +44,32 @@ const Login = () => {
               <input
                 type="text"
                 placeholder="Full Name"
-                className="p-4 m-2 bg-[#000000c9] border-[1px] border-gray-700 rounded-md"
+                className="p-4 m-2 bg-[#1010129c] border-[1px] border-gray-700 rounded-md"
               />
             )}
             <input
-              type="text"
-              placeholder="Email or mobile number "
-              className="p-4  m-2 bg-[#000000c9] border-[1px] border-gray-700 rounded-md"
+              ref={emailRef}
+              type="email"
+              placeholder="Email"
+              className="p-4  m-2 bg-[#1010129c] border-[1px] border-gray-700 rounded-md"
             />
             <input
-              type="text"
+              ref={passwordRef}
+              type="password"
               placeholder="Password"
-              className="p-4 m-2 bg-[#000000c9] border-[1px] border-gray-700 rounded-md"
+              className="p-4 m-2 bg-[#1010129c] border-[1px] border-gray-700 rounded-md"
             />
-            <button className="p-2 m-2  bg-red-600 hover:bg-red-700 transition-all hover:transition-all rounded-md">
+            <p className="text-red-600 px-2">{error}</p>
+            <button
+              onClick={handleButtonClick}
+              className="p-2 m-2  bg-red-600 hover:bg-red-700 transition-all hover:transition-all rounded-md"
+            >
               Sign In
             </button>
 
             <button className="px-2 py-1 m-2">Forgot Password?</button>
             <h1 className="mx-2 my-1 px-1">
-              <input type="checkbox" className="transition-all" /> Remember me
+              {/* <input type="checkbox" className="transition-all" /> Remember me */}
             </h1>
             <h1
               className="mx-2 px-1 cursor-pointer transition-all duration-1000"
