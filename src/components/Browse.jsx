ce4bedb0cff843bab05eea9c7c4f2c745c7125f6
Adnan <ak567845@gmail.com>
2024-11-06 09:17:32 +0530

    removed loader spinner

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -18,19 +18,19 @@ const Browse = () => {
   useUpcomingMovies();
 
   const loader = useSelector((store) => store.movies.nowPlayingMovies);
-  if (!loader)
-    return (
-      <div className="nfLoader flex flex-col text-white gap-5 text-xl justify-center  bg-black items-center fixed inset-0">
-        <div
-          className="w-16 h-16 bg-center bg-no-repeat bg-contain animate-spin-slow"
-          style={{
-            backgroundImage:
-              "url(https://assets.nflxext.com/en_us/pages/wiplayer/site-spinner.png)",
-          }}
-        ></div>
-        Loading....
-      </div>
-    );
+  // if (!loader)
+  //   return (
+  //     <div className="nfLoader flex flex-col text-white gap-5 text-xl justify-center  bg-black items-center fixed inset-0">
+  //       <div
+  //         className="w-16 h-16 bg-center bg-no-repeat bg-contain animate-spin-slow"
+  //         style={{
+  //           backgroundImage:
+  //             "url(https://assets.nflxext.com/en_us/pages/wiplayer/site-spinner.png)",
+  //         }}
+  //       ></div>
+  //       Loading....
+  //     </div>
+  //   );
 
   return (
     <div className="font-DMSANS">
